02ee125234141289117bf60fee64fbb01a523335
Bleppish <whyismynametakenagain2@gmail.com>
2025-04-24 23:40:03 +0300

    something exists now x2

--- server.js
+++ server.js
@@ -2,23 +2,33 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-let votes = JSON.parse(fs.readFileSync('votes.json'));
+let db = {
+  elections: [{ id: 1, name: "Class President" }],
+  votes: []
+};
 
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/election', (req, res) => { 
-  res.json([{ id: 1, name: "Class President" }]);
+app.get('/elections', (req, res) => {
+  res.json(db.elections); 
 });
 
 app.post('/vote', (req, res) => {
-  votes.push(req.body); 
-  fs.writeFileSync('votes.json', JSON.stringify(votes));
-  res.send("Vote submitted (maybe)");
+  if (!req.body.electionId || !req.body.choice) { 
+    return res.status(400).send("Missing data");
+  }
+  db.votes.push(req.body);
+  saveDb();
+  res.send("Vote recorded!");
 });
 
-app.get('/admin/results', (req, res) => {
-  res.json(votes); 
+app.get('/results', (req, res) => {
+  res.json(db.votes); 
 });
 
-app.listen(3000, () => console.log("Server running (probably)"));
+function saveDb() {
+  fs.writeFileSync('votes.db.json', JSON.stringify(db));
+}
+
+app.listen(3000, () => console.log("Server started on port 3000"));
